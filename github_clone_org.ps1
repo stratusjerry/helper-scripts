0c7daeecd26f970436587402cabecf9c0e828c17
stratusjerry <icandevops@gmail.com>
2022-01-27 13:57:18 -0500

    add local repo check

--- github_clone_org.ps1
+++ github_clone_org.ps1
@@ -1,20 +1,33 @@
 # Git clone all the repos under an Organization/User
 $org = "GoogleContainerTools"
+$skipLocal = $true
 
 # Fix for Powershell default to TLSv1.0
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls, [Net.SecurityProtocolType]::Tls11, [Net.SecurityProtocolType]::Tls12, [Net.SecurityProtocolType]::Ssl3
 [Net.ServicePointManager]::SecurityProtocol = "Tls, Tls11, Tls12, Ssl3"
 
-$pageCount = 1; $perPage = 100
+$subDirs = Get-ChildItem -Directory -Name
+$userURL = "https://api.github.com/users/" + $org
+$reqUser = Invoke-RestMethod $userURL
+$repos = $reqUser.public_repos
+$pageCount = 0; $perPage = 100; $repoCount = 0
 Do {
+    $pageCount += 1
     $pagedURL = "https://api.github.com/orgs/" + $org + "/repos?page=" + $pageCount + "&per_page=" + $perPage
     $request = Invoke-WebRequest -Uri $pagedURL
     $reqJson = ConvertFrom-Json ($request.Content)
     #$reqJson = ConvertFrom-Json $([String]::new($request.Content)) #This is nicer
     foreach ($item in $reqJson){
+        $repoCount += 1
         $URL = $item.clone_url
-        Write-Host "Cloning: $URL" -ForegroundColor Green
+        if ($skipLocal){
+            $localDir = $URL -replace "https://github.com/$org/(.*)\.git",'$1'
+            if ( $subDirs.Contains($localDir) ){
+                Write-Host "($repoCount / $repos) Skipping Local: $localDir" -ForegroundColor Red
+                continue
+            }
+        }
+        Write-Host "($repoCount / $repos) Cloning: $URL" -ForegroundColor Green
         git clone $URL
     }
-    $pageCount += 1
 } While ( $request.Headers.Link.Contains("next") )
